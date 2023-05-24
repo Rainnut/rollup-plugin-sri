6c128d83726d874c0e8c317576228b3a383dcedf
zhangdongyang <zhangdongyang@joyy.com>
2023-05-24 19:11:57 +0800

    feat: not working

--- index.ts
+++ index.ts
@@ -1,24 +1,3 @@
-////////////////////////////////////////////////////////////////////////////////
-//
-// @small-tech/vite-plugin-sri
-//
-// Subresource integrity (SRI) plugin for Vite (https://vitejs.dev/)
-//
-// Adds subresource integrity hashes to script and stylesheet
-// imports from your index.html file at build time.
-//
-// If you’re looking for a generic Rollup plugin that does the same thing,
-// see rollup-plugin-sri by Jonas Kruckenberg that this one was inspired by:
-// https://github.com/JonasKruckenberg/rollup-plugin-sri
-//
-// Like this? Fund us!
-// https://small-tech.org/fund-us
-//
-// Copyright ⓒ 2021-present Aral Balkan, Small Technology Foundation
-// License: ISC.
-//
-////////////////////////////////////////////////////////////////////////////////
-
 import { Element, load } from 'cheerio'
 import { Plugin, ResolvedConfig } from 'vite'
 // import fetch from 'node:node-fetch'
@@ -28,19 +7,21 @@ import { resolve } from 'path'
 import { OutputBundle } from 'rollup'
 
 export default function sri(): Plugin {
+  // rollup.config 必须填options，vite自动获取options
   let config: ResolvedConfig
   const bundle: OutputBundle = {}
 
   return {
-    name: 'vite-plugin-sri',
+    name: 'rollup-plugin-sri',
+    // vite 才会调用configResolved初始化配置
     configResolved(resolvedConfig) {
       config = resolvedConfig
     },
-    enforce: 'post',
-    apply: 'build',
-    async writeBundle(options, _bundle) {
+    // enforce: 'post',
+    // apply: 'build',
+    async writeBundle(writeBundleOptions, _bundle) {
       // when use with vite-plugin-legacy
-      // writeBundle will be called twice
+      // writeBundle 调用俩次
       // legacy bundle will be run first, but not with index.html file
       // esm bundle will be run after, so should saved legacy bundle before esm bundle output.
       Object.entries(_bundle).forEach(([k, v]) => {
@@ -49,9 +30,9 @@ export default function sri(): Plugin {
       })
 
       const htmls = Object.keys(bundle)
-        .filter(filename => filename.endsWith('.html'))
+        .filter((filename) => filename.endsWith('.html'))
         // @ts-ignore
-        .map(filename => {
+        .map((filename) => {
           const bundleItem = bundle[filename]
           if (bundleItem.type === 'asset') {
             return {
@@ -60,16 +41,16 @@ export default function sri(): Plugin {
             }
           }
         })
-        .filter(item => !!item) as Array<{
-          name: string
-          source: string
-        }>
+        .filter((item) => !!item) as Array<{
+        name: string
+        source: string
+      }>
 
       htmls.forEach(async ({ name, source: html }) => {
         // @ts-ignore
         const $ = load(html as string)
 
-        // Implement SRI for scripts and stylesheets.
+        // 给 scripts、stylesheets 标签添加 SRI
         const scripts = $('script').filter('[src]')
         const stylesheets = $('link').filter('[href]')
 
@@ -88,10 +69,11 @@ export default function sri(): Plugin {
           )?.[1]
 
           if (!t) {
-            config.logger.warn(`cannot find ${resourcePath} in output bundle.`)
+            const logger = config.logger || this
+            logger.warn(`cannot find ${resourcePath} in output bundle.`)
             try {
               source = readFileSync(
-                resolve(options.dir as string, resourcePath)
+                resolve(writeBundleOptions.dir as string, resourcePath)
               )
             } catch (error) {
               source = void 0
@@ -110,7 +92,7 @@ export default function sri(): Plugin {
               .digest()
               .toString('base64')}`
 
-          if (element.attribs.crossorigin === void 0) {
+          if (element.attribs.integrity && !element.attribs.crossorigin) {
             // 在进行跨域资源请求时，integrity必须配合crossorigin使用，不然浏览器会丢弃这个资源的请求
             // https://developer.mozilla.org/zh-CN/docs/Web/HTML/Attributes/crossorigin
             element.attribs.crossorigin = 'anonymous'
@@ -127,7 +109,7 @@ export default function sri(): Plugin {
         ])
 
         writeFileSync(
-          resolve(config?.root as string, config?.build.outDir as string, name),
+          resolve(process.cwd(), config?.build.outDir as string, name),
           $.html()
         )
       })
